a49edf4582779e4859396e11b453f773d0d63fb7
Renan <renan.tsx@gmail.com>
2024-03-21 03:36:06 -0300

    Config Jest Runner

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import { compilerOptions } from './tsconfig.json'
 
 // NOTE moduleNameMapper interplet the paths
 
+// NOTE Config Jest Runner
+// 1 - Install plugin VSCode Jest Runner
+// 2 - VSCode settings: Jest Runner: Code Lens Selector = **/*.{test,spec,int-spec}.{js,jsx,ts,tsx}
+
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
