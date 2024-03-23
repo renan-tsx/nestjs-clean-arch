9bbea924591f76fad9772f098b634bc950805f0d
Renan <renan.tsx@gmail.com>
2024-03-23 12:58:46 -0300

    UserEntity - Atualização dos métodos

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -16,17 +16,41 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createAt = this.props.createAt ?? new Date()
   }
 
+  // NOTE name
+
   get name() {
     return this.props.name
   }
+
+  updateName(value: string): void {
+    this.name = value
+  }
+
+  private set name(value: string) {
+    this.props.name = value
+  }
+
+  // NOTE email
 
   get email() {
     return this.props.email
   }
 
+  // NOTE password
+
   get password() {
     return this.props.password
   }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
+  private set password(value: string) {
+    this.props.password = value
+  }
+
+  // NOTE createAt
 
   get createAt() {
     return this.props.createAt
