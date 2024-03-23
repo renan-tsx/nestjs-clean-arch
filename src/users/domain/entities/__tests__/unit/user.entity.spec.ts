65b3c910ec196f28b896621450026bd2b64fe36f
Renan <renan.tsx@gmail.com>
2024-03-23 13:13:45 -0300

    UserEntity - Atualizado testes unitários

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -22,6 +22,12 @@ describe('Testes unitários da UserEntity', () => {
     expect(typeof sut.props.name).toBe('string')
   })
 
+  it('Atribuir valor ao campo name', () => {
+    sut['name'] = 'algum nome'
+    expect(sut.props.name).toEqual('algum nome')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
   it('Obter campo email', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.props.email).toEqual(props.email)
@@ -34,9 +40,27 @@ describe('Testes unitários da UserEntity', () => {
     expect(typeof sut.props.password).toBe('string')
   })
 
+  it('Atribuir valor ao campo password', () => {
+    sut['password'] = 'algum password'
+    expect(sut.props.password).toEqual('algum password')
+    expect(typeof sut.props.password).toBe('string')
+  })
+
   it('Obter campo createAt', () => {
     expect(sut.props.createAt).toBeDefined()
     expect(sut.props.createAt).toEqual(props.createAt)
     expect(sut.props.createAt).toBeInstanceOf(Date)
+  })
+
+  it('Atualizar o campo name', () => {
+    sut.updateName('atualizado name')
+    expect(sut.props.name).toEqual('atualizado name')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Atualizar o campo password', () => {
+    sut.updatePassword('atualizado password')
+    expect(sut.props.password).toEqual('atualizado password')
+    expect(typeof sut.props.password).toBe('string')
   })
 })
