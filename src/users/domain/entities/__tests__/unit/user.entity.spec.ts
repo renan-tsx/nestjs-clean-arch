9f1c631718d4379ec27ff64fbc0388d80abec145
Renan <renan.tsx@gmail.com>
2024-03-23 02:48:19 -0300

    UserEntity - Teste unitários do métodos get

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { UserEntity, UserProps } from '../../user.entity'
 
-describe('Testes unitários UserEntity', () => {
+describe('Testes unitários da UserEntity', () => {
   let props: UserProps
   let sut: UserEntity
 
@@ -18,6 +18,30 @@ describe('Testes unitários UserEntity', () => {
     expect(sut.props.name).toEqual(props.name)
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createAt).toBeInstanceOf(Date)
+  })
+
+  it('Obter campo name', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Obter campo email', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('Obter campo password', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Obter campo createAt', () => {
+    expect(sut.props.createAt).toBeDefined()
+    expect(sut.props.createAt).toEqual(props.createAt)
     expect(sut.props.createAt).toBeInstanceOf(Date)
   })
 })
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createAt = this.props.createAt ?? new Date()
   }
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  get createAt() {
+    return this.props.createAt
+  }
 }
